ec0db30279010be45b2bc655d37b47e3e9440486
cotjdud051008 <cotjdud051008@naver.com>
2025-01-06 16:52:16 +0900

    4장 실습문제, 셀프체크

---
+++ 4/problem.py
@@ -0,0 +1,35 @@
+s = 'http://naver.com'
+
+s1 = s[7:]
+s1 = s1[0:-4]
+
+# print(s1)
+
+print(f'{s}의 비밀번호는 {s1[0:3]}{len(s1)}{s.count('e')}! 입니다.')
+
+s = 'http://google.com'
+
+s1 = s[7:]
+s1 = s1[0:-4]
+
+# print(s1)
+
+print(f'{s}의 비밀번호는 {s1[0:3]}{len(s1)}{s.count('e')}! 입니다.')
+
+s = 'http://daum.net'
+
+s1 = s[7:]
+s1 = s1[0:-4]
+
+# print(s1)
+
+print(f'{s}의 비밀번호는 {s1[0:3]}{len(s1)}{s.count('e')}! 입니다.')
+
+s = 'http://youtube.com'
+
+s1 = s[7:]
+s1 = s1[0:-4]
+
+# print(s1)
+
+print(f'{s}의 비밀번호는 {s1[0:3]}{len(s1)}{s.count('e')}! 입니다.')
---
+++ 4/selfcheck.py
@@ -0,0 +1,20 @@
+### 주어진 영어 문장의 첫 글자는 대문자로, 나머지 글자는 소문자로 변환
+
+# 주어진 문장 : the early bird catches the worm.
+
+sen = 'the early bird catches the worm.'
+
+print(sen.capitalize())
+
+# 주어진 문장 : Actions Speak Louder Than Words.
+
+sen = 'Actions Speak Louder Than Words.'
+
+print(sen.capitalize())
+
+# 주어진 문장 : PRACTICE MAKES PERFECT.
+
+sen = 'PRACTICE MAKES PERFECT.'
+
+print(sen.capitalize())
+
